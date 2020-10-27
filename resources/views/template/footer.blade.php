9c2a0b04d27ac10058e70bd28fdf22595eee1e11
Sandy Sihotang <sandysihotang12@gmail.com>
2020-10-27 19:01:33 +0700

    fix footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,1 +1,14 @@
+<footer class="blog-footer" style="background-color: white;">
+    <div class="row justify-content-center">
+        <p class="text-muted">
+            KEMENTERIAN PARIWISATA DAN EKONOMI KREATIF REPUBLIK INDONESIA | 2020
+        </p>
+    </div>
+    <div class="row justify-content-center">
+        <ul class="list-unstyled socila-list">
+            <li><img src="/image/logo/Logo_Kemenparekraf.png" style="height: 48px; width: 48px"/></li>
+            <li><img src="/image/logo/Logo_ITDel.png" style="height: 48px; width: 48px" alt=""/></li>
+        </ul>
+    </div>
+</footer>
 </html>
