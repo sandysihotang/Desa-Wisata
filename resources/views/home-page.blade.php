127668db505b25fb83e5ac567717958bebd7e16b
Desy <desy.isabel97@gmail.com>
2020-11-03 20:31:54 +0700

    perbaikan home

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -1,10 +1,11 @@
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 @include('template.header')
 <div class="row" id="homes">
     <q-img src="/image/home/DSC01411.JPG" style="max-height: 400px !important; width: 100%;">
         <div class="absolute-bottom-right col-md-5 text-subtitle2 justify-content-center">
             <div class="float-right">
                 <div class="row">
-                    <div class="text-uppercase"><h5>Restoran Terapung Di Bakkara</h5></div>
+                    <div class="sub-title">Restoran Terapung Di Bakkara</div>
                 </div>
                 <div class="row">
                     <p>Restoran Terapung bernama tipang mas menyuguhkan lokasi tenang dengan pemandangan danau dan
@@ -19,57 +20,55 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <h4 class="text-uppercase">BERITA TERBARU</h4>
+                <div class="sub-title">BERITA TERBARU</div>
             </div>
         </div>
-        <div class="row" style="background-color: #f7f7f7">
+        <div class="row background">
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/Bakara_Humbang_Hasundutan_6938.jpg">
+                            <img src="/image/image/04_Istana_Sisingamangaraja_Riyanthi_Sianturi.jpg" class="card-img2">
 
-                            <q-card-section>
-                                <div class="text-h6">ARTEFAK KERAJAAN SISINGAMANGARAJA</div>
-                                <div class="text-subtitle3">Kita dapat melihat makam Raja Sisingamangaraja XI dilokasi
+                            <div class="container">
+                                <div class="card-title-home">Artefak Kerajaan Sisingamangaraja</div>
+                                <div class="card-caption-home">Kita dapat melihat makam Raja Sisingamangaraja XI di lokasi
                                     ini, termasuk rumah adat batak lengkap dengan gorga-nya yang mewah
                                 </div>
-                            </q-card-section>
+                            </div>
 
                             <q-card-section class="q-pt-none">
-                                KATEGORI WISATA/ SENI DAN BUDAYA
+                                KATEGORI WISATA / SENI DAN BUDAYA
                             </q-card-section>
                         </q-card>
                     </div>
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/DSC01372.JPG">
+                            <img src="/image/image/04_Pantai_Pardinggaran_Toba_Riyanthi_Sianturi-1.JPG" class="card-img2">
 
                             <q-card-section>
-                                <div class="text-h6">ARTEFAK KERAJAAN SISINGAMANGARAJA</div>
-                                <div class="text-subtitle3">Kita dapat melihat makam Raja Sisingamangaraja XI dilokasi
-                                    ini, termasuk rumah adat batak lengkap dengan gorga-nya yang mewah
+                                <div class="card-title-home">Matahari Terbit di Danau Toba</div>
+                                <div class="card-caption-home">Pemandangan Danau Toba yang luar biasa bisa dinikmati di sini. Pada lokasi ini kta bisa memandang Pulau Sibandang, Muara, Balige dan Samosir di kejauhan.
                                 </div>
                             </q-card-section>
 
                             <q-card-section class="q-pt-none">
-                                KATEGORI WISATA/ SENI DAN BUDAYA
+                                GALERI   /   KELUARGA
                             </q-card-section>
                         </q-card>
                     </div>
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/DSC01536.JPG">
+                            <img src="/image/image/02_Air_Terjun_Janji_Riyanthi_Sianturi.JPG" class="card-img2">
 
                             <q-card-section>
-                                <div class="text-h6">ARTEFAK KERAJAAN SISINGAMANGARAJA</div>
-                                <div class="text-subtitle3">Kita dapat melihat makam Raja Sisingamangaraja XI dilokasi
-                                    ini, termasuk rumah adat batak lengkap dengan gorga-nya yang mewah
+                                <div class="card-title-home">Air Terjun Janji</div>
+                                <div class="card-caption-home">Selain menikmati percikan airnya saat berfoto, kita bisa mandi di area pemandian dengan air yang segar.
                                 </div>
                             </q-card-section>
 
                             <q-card-section class="q-pt-none">
-                                KATEGORI WISATA/ SENI DAN BUDAYA
+                                KATEGORI WISATA   /   ADVENTURE
                             </q-card-section>
                         </q-card>
                     </div>
@@ -82,7 +81,7 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
-                <h4 class="text-uppercase">WISATA DESA</h4>
+                <div class="sub-title">WISATA DESA</div>
             </div>
         </div>
         <div class="row" style="background-color: #f7f7f7">
@@ -90,14 +89,12 @@
                 <div class="row">
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/DSC01408.JPG">
+                            <img src="/image/image/IMG_3684.JPG" class="card-img2">
 
                             <q-card-section>
-                                <div class="text-h6">PUNCAK GOTTING</div>
-                                <div class="text-subtitle3">
-                                    Puncak gotting berada diketinggian, tempat bersantai sambil
-                                    memandang Danau Toba yang luas dari berbagai sisi.
-                                    jika ingin sambil berolahraga, dapat berjalan kaki atau sepeda
+                                <div class="card-title-home">Puncak Gotting</div>
+                                <div class="card-caption-home">
+                                    Puncak Gotting berada di ketinggian, tempat bersantai sambil memandang Danau Toba yang luas dari berbagai sisi. Jika ingin sambil berolahraga, dapat berjalan kaki atau bersepeda.
                                 </div>
                             </q-card-section>
 
@@ -108,37 +105,33 @@
                     </div>
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/DSC01476.JPG">
+                            <img src="/image/image/IMG_3668.JPG" class="card-img2">
 
                             <q-card-section>
-                                <div class="text-h6">PUNCAK GOTTING</div>
-                                <div class="text-subtitle3">
-                                    Puncak gotting berada diketinggian, tempat bersantai sambil
-                                    memandang Danau Toba yang luas dari berbagai sisi.
-                                    jika ingin sambil berolahraga, dapat berjalan kaki atau sepeda
+                                <div class="card-title-home">Terasering Desa</div>
+                                <div class="card-caption-home">
+                                    Kegiatan menanam padi menjadi tradisi yang dapat dijadikan wisata keluarga. Tiap keluarga dapat bermain sambil belajar menanam padi bersama dengan para petani.
                                 </div>
                             </q-card-section>
 
                             <q-card-section class="q-pt-none">
-                                WISATA DESA / ADVENTURE
+                                WISATA DESA  /   KELUARGA
                             </q-card-section>
                         </q-card>
                     </div>
                     <div class="col-md-4 mt-4 mb-4">
                         <q-card class="my-card">
-                            <img src="/image/home/DSC01411.JPG">
+                            <img src="/image/image/IMG_3650.JPG" class="card-img2">
 
                             <q-card-section>
-                                <div class="text-h6">PUNCAK GOTTING</div>
-                                <div class="text-subtitle3">
-                                    Puncak gotting berada diketinggian, tempat bersantai sambil
-                                    memandang Danau Toba yang luas dari berbagai sisi.
-                                    jika ingin sambil berolahraga, dapat berjalan kaki atau sepeda
+                                <div class="card-title-home">Lembah Bakara</div>
+                                <div class="card-caption-home">
+                                    Pemandangan aliran sungai Aek Silang yang mengarah ke Danau Toba, perumahan penduduk dan persawarahan yang dikelilingi oleh jajaran perbukitan hijau, membentuk pemandangan indah Lembah Bakara.
                                 </div>
                             </q-card-section>
 
                             <q-card-section class="q-pt-none">
-                                WISATA DESA / ADVENTURE
+                                WIISATA DESA  /   ALAM
                             </q-card-section>
                         </q-card>
                     </div>
