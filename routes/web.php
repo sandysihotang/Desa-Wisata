a059d63332efefb206f7275b4e18ec326d0acd1b
Desy <desy.isabel97@gmail.com>
2020-11-17 11:27:47 +0700

    fasilitas

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::get('/profil-desa/{data}', [App\Http\Controllers\ProfilDesaController::cl
 
 //Fasilitas
 Route::get('/fasilitas-desa/{data}', [App\Http\Controllers\FasilitasDesaController::class, 'view']);
+
 Route::get('/detail-fasilitas/{id}', [FasilitasDesaController::class, 'getFasilitas']);
 // Route::get('/fasilitas-pariwisata', function () {
 //     return view('fasilitas-pariwisata');
