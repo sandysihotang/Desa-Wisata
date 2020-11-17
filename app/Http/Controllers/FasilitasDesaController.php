1bf9a04c0fa2eb7c5ec1abc8a32c734fbaaebbab
Sandy Sihotang <sandysihotang12@gmail.com>
2020-11-17 15:37:37 +0700

    solve

--- app/Http/Controllers/FasilitasDesaController.php
+++ app/Http/Controllers/FasilitasDesaController.php
@@ -43,7 +43,7 @@ class FasilitasDesaController extends Controller
         $newFasilitas = new FasilitasDesa();
         $newFasilitas->nama_fasilitas = $request->title;
         $newFasilitas->deskripsi = $request->story;
-        
+
         $newFasilitas->save();
 
 
@@ -63,7 +63,7 @@ class FasilitasDesaController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -74,7 +74,7 @@ class FasilitasDesaController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -85,7 +85,7 @@ class FasilitasDesaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -96,8 +96,8 @@ class FasilitasDesaController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function updateFasilitas(Request $request, $id)
@@ -115,7 +115,7 @@ class FasilitasDesaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function deleteFasilitas($id)
