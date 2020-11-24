56806b8ee2a86cffdee9cf855364a035c723f093
Sandy Sihotang <sandysihotang12@gmail.com>
2020-11-24 17:34:17 +0700

    search galeri

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -213,7 +213,7 @@
         </header>
 
         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light justify-content-between" id="menus">
+            <nav class="navbar navbar-expand-lg navbar-light" id="menus">
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
