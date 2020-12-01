29260e337716d1406ad7fdcd551dd4920e59aa53
Sandy Sihotang <sandysihotang12@gmail.com>
2020-12-01 16:45:15 +0700

    change editor

--- resources/js/components/Menu/Menu.vue
+++ resources/js/components/Menu/Menu.vue
@@ -1,16 +1,15 @@
 <template>
-    <div v-if="success_get">
-        <div class="title">{{ res.judul_halaman}}</div>
+    <div>
+        <div class="title" v-if="success_get">{{ res.judul_halaman}}</div>
         <div class="row">
             <div class="container background">
-                <ckeditor :editor="editor" :disabled="true"  v-model="res.isi_halaman" :config="editorConfig"></ckeditor>
+                <div id="editor" v-html="res.isi_halaman"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import CKEditorClassic from '@ckeditor/ckeditor5-build-balloon-block'
     import UploadAdapter from "../../UploadAdapter";
 
     export default {
@@ -19,10 +18,6 @@
                 res: [],
                 success_get: false,
                 isEdit: false,
-                editor: CKEditorClassic,
-                editorConfig: {
-                    extraPlugins: [this.uploader],
-                },
             }
         },
         methods: {
@@ -37,11 +32,23 @@
                 axios.get(`/get-isi-menu/${id}`)
                     .then(e => {
                         this.res = e.data
+                        this.construct()
                         this.success_get = true
                     })
                     .catch(e => {
                         alert('Koneksi kurang stabil, silahkan refresh halaman')
                     })
+            },
+            construct() {
+                BalloonEditor.create(document.querySelector('#editor'))
+                    .then(editor => {
+                        window.editor = editor;
+                        window.editor.isReadOnly  = true
+                        window.editor.extraPlugins = [this.uploader(editor)]
+                    })
+                    .catch(error => {
+                        console.error('There was a problem initializing the editor.', error);
+                    });
             },
         },
         mounted() {
